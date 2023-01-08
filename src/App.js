7185e1b50b68f453500e4a8836493e1948ca100a
rohitgirotra <rohit.girotra@nagarro.com>
2023-01-08 21:34:14 +0530

    [git-crash-2023] routing start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import { Alert } from "./components/layout/Alert";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
 
