0b0f031f85fd8d90a357369f726001910b84dafb
rohitgirotra <rohit.girotra@nagarro.com>
2023-01-15 23:19:36 +0530

    [git-crash-2023]-Class Based Final Commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import About from "./components/pages/About";
+import User from "./components/users/User";
 import { Alert } from "./components/layout/Alert";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
 
 class App extends Component {
      state = {
           users: [],
+          user: {},
           loading: false,
           alert: null,
+          repos: [],
      };
 
      async componentDidMount() {
@@ -29,6 +34,22 @@ class App extends Component {
           this.setState({ users: response.data.items, loading: false });
      };
 
+     getUser = async (username) => {
+          this.setState({ loading: true });
+          const response = await axios.get(
+               `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+          );
+          this.setState({ user: response.data, loading: false });
+     };
+
+     getUserRepos = async (username) => {
+          this.setState({ loading: true });
+          const response = await axios.get(
+               `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+          );
+          this.setState({ repos: response.data, loading: false });
+     };
+
      clearUsers = () => this.setState({ users: [], loading: false });
 
      setAlert = (msg, type) => {
@@ -37,21 +58,45 @@ class App extends Component {
      };
 
      render() {
-          const { users, loading } = this.state;
+          const { users, user, loading, repos } = this.state;
           return (
-               <div className="App">
-                    <Navbar />
-                    <div className="container">
-                         <Alert alert={this.state.alert} />
-                         <Search
-                              searchUsers={this.searchUsers}
-                              clearUsers={this.clearUsers}
-                              showClear={users.length > 0 ? true : false}
-                              setAlert={this.setAlert}
-                         />
-                         <Users loading={loading} users={users} />
+               <Router>
+                    <div className="App">
+                         <Navbar />
+                         <div className="container">
+                              <Alert alert={this.state.alert} />
+                              <Routes>
+                                   <Route
+                                        path="/"
+                                        element={
+                                             <Fragment>
+                                                  <Search
+                                                       searchUsers={this.searchUsers}
+                                                       clearUsers={this.clearUsers}
+                                                       showClear={users.length > 0 ? true : false}
+                                                       setAlert={this.setAlert}
+                                                  />
+                                                  <Users loading={loading} users={users} />
+                                             </Fragment>
+                                        }
+                                   />
+                                   <Route path="/about" element={<About />} />
+                                   <Route
+                                        path={`/user/:login`}
+                                        element={
+                                             <User
+                                                  getUser={this.getUser}
+                                                  getUserRepos={this.getUserRepos}
+                                                  user={user}
+                                                  repos={repos}
+                                                  loading={loading}
+                                             />
+                                        }
+                                   />
+                              </Routes>
+                         </div>
                     </div>
-               </div>
+               </Router>
           );
      }
 }
